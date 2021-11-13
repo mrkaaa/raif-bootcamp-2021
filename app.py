e62bb6e57f366c76652a602a4ad301f1f09eacde
Anton Gavrilov <anton.gavrilov@raiffeisen.ru>
2021-11-13 03:35:34 +0300

    GPT Model

--- app.py
+++ app.py
@@ -4,24 +4,35 @@ import typing
 
 from flask import Flask, request
 
-from models import RuBertModel
+from models import GPTModel, RuBertModel
 
 
 logging.basicConfig(level=logging.INFO)
 
 APP: Flask = Flask(__name__)
-MODEL: RuBertModel = RuBertModel()
+MODEL: GPTModel = GPTModel("sberbank-ai/rugpt3large_based_on_gpt2")
 LOGGER: logging.Logger = logging.getLogger("RaifGPT-3")
 
 
 @APP.route("/predict", methods=["POST"])
 def predict():
     data: dict = request.form
-
+    available_help = set(data["available help"])
     question: str = data["question"]
-    variants: typing.List[str] = [data["answer_1"], data["answer_2"], data["answer_3"], data["answer_4"]]
+    variants: typing.List[str] = [data[x] for x in ["answer_1", "answer_2", "answer_3", "answer_4"] if data[x]]
 
-    return random.choice([{"answer": MODEL.predict(variants, question)}, {"end game": "take money"}])
+    prediction, score = MODEL.predict(variants, question)
+
+    if score > 28 and data['question money'] != 2000:
+        if "new question" in available_help:
+            return {"help": "new question"}
+        if "can mistake" in available_help:
+            return {"answer": prediction, "help": "can mistake"}
+        if "fifty fifty" in available_help:
+            return {"help": "fifty fifty"}
+        return {"end game": "take money"}
+
+    return {"answer": prediction}
 
 
 @APP.route("/result_question", methods=["POST"])
